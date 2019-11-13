76df0aaa7c56e8d8e5248d6d992ff6a17b637477
Alethea Cary <aletheacary@gmail.com>
2019-11-13 11:17:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,8 +6,9 @@ def display_board
   row2 = ["   ", "   ", "   "]
   row3 = ["   ", "   ", "   "]
   dashes = "-----------"
-  board = [row1, dashes, row2, dashes, row3]
+  board = [row1, row2, row3]
   board.each do |row|
     puts row.join("|")
+    puts dashes
   end
 end
